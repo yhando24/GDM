c689ca19b253719ce609cb6aa431a763de516b7e
yhando24 <youcef.handoura@gmail.com>
2019-03-11 10:17:00 +0100

    base projet

---
+++ src/main/java/fr/diginamic/GDM/GdmApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.GDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdmApplication.class, args);
+	}
+
+}
