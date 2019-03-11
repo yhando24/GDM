bf02a29359a0872076eb2ea6f61b3eb72a03f5f1
Delteil <a.delteil83@gmail.com>
2019-03-11 15:29:20 +0100

    commit Amandine

--- src/main/java/fr/diginamic/GDM/GdmApplication.java
+++ src/main/java/fr/diginamic/GDM/GdmApplication.java
@@ -2,8 +2,10 @@ package fr.diginamic.GDM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="fr.diginamic")
 public class GdmApplication {
 
 	public static void main(String[] args) {
