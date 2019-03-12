be3fd24b65ff630c747dacc76eb1ae18ddbf61cd
yoann <yoann@merca.com>
2019-03-12 10:23:41 +0100

    add mapstruct in pom

--- src/main/java/fr/diginamic/expenseAccount/repository/ExpenseAccountRepository.java
+++ src/main/java/fr/diginamic/expenseAccount/repository/ExpenseAccountRepository.java
@@ -1,8 +1,18 @@
 package fr.diginamic.expenseAccount.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ExpenseAccountRepository {
+import fr.diginamic.expenseAccount.model.ExpenseAccount;
+import fr.diginamic.expenseAccount.model.ExpenseAccountStatusEnum;
+import fr.diginamic.mission.model.Mission;
 
+@Repository
+public interface ExpenseAccountRepository extends CrudRepository<ExpenseAccount, Long>{
+	
+	List<ExpenseAccount> findByMission(Mission mission);
+	List<ExpenseAccount> findByStatus(ExpenseAccountStatusEnum status);
+	
 }
