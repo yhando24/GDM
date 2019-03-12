0c51ded3c282479d42fb01a8c2212deb88972f54
yhando24 <youcef.handoura@gmail.com>
2019-03-12 15:51:53 +0100

    plop je suis youcef

---
+++ src/main/java/fr/diginamic/mission/exception/ErrorLogigDateMission.java
@@ -0,0 +1,27 @@
+package fr.diginamic.mission.exception;
+
+public class ErrorLogigDateMission extends Exception {
+		
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6843167865090022378L;
+	
+	
+	private String message;
+
+
+	public ErrorLogigDateMission(String message) {
+		super();
+		this.message = message;
+	}
+	
+
+	public ErrorLogigDateMission( ) {
+
+	}
+	
+	
+	
+	
+}
