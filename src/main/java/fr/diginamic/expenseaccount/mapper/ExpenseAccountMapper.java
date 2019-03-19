5b20f34a868868e67eb09af70aa4524005118ea6
Delteil <a.delteil83@gmail.com>
2019-03-19 12:36:29 +0100

    update Mapper expanseAccount

--- src/main/java/fr/diginamic/expenseaccount/mapper/ExpenseAccountMapper.java
+++ src/main/java/fr/diginamic/expenseaccount/mapper/ExpenseAccountMapper.java
@@ -3,11 +3,14 @@ package fr.diginamic.expenseaccount.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import fr.diginamic.expenseaccount.model.ExpenseAccount;
+import fr.diginamic.expenseaccount.model.ExpenseAccountDTO;
+
 @Mapper
-public class ExpenseAccountMapper {
+public interface ExpenseAccountMapper {
 
 	ExpenseAccountMapper INSTANCE = Mappers.getMapper(ExpenseAccountMapper.class);
 
-	// ExpenseAccountDTO expenseAccountToExpenseAccountDTO(ExpenseAccount e);
+	 ExpenseAccountDTO expenseAccountToExpenseAccountDTO(ExpenseAccount expenseAccount);
 
 }
