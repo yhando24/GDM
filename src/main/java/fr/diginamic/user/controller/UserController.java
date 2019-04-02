040a2a11d49fe47857891ebd3eddcbe4bdefa784
unknown <alex.ayato@gmail.com>
2019-04-02 11:19:27 +0200

    UserController modif

--- src/main/java/fr/diginamic/user/controller/UserController.java
+++ src/main/java/fr/diginamic/user/controller/UserController.java
@@ -1,5 +1,38 @@
 package fr.diginamic.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.diginamic.user.model.User;
+import fr.diginamic.user.model.UserDTO;
+import fr.diginamic.user.service.UserService;
+
+@CrossOrigin
+@RestController()
+@RequestMapping("/users")
 public class UserController {
+
+	@Autowired
+	private UserService userServ;
+
+	@GetMapping
+	public List<User> findAll() {
+		return this.userServ.findAll();
+	}
+
+	@PostMapping
+	@Transactional
+	public User verification(@RequestBody UserDTO userDTO) {
+		return userServ.save(userDTO);
+
+	}
 
 }
