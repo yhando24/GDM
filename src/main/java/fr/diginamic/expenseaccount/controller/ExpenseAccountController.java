d56a2c5a5d7fbc3154778519644f89c293d2ae4b
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-04-09 12:04:44 +0200

    ajout delete et upgrate a note de frais

--- src/main/java/fr/diginamic/expenseaccount/controller/ExpenseAccountController.java
+++ src/main/java/fr/diginamic/expenseaccount/controller/ExpenseAccountController.java
@@ -2,9 +2,16 @@ package fr.diginamic.expenseaccount.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +31,20 @@ public class ExpenseAccountController {
 	public List <ExpenseAccountDTO> findAll(){
 		return this.expenseAccountService.findAll();
 	}
+
+	@Transactional
+	@PostMapping
+	public ExpenseAccountDTO saveExpenseAccount(@RequestBody ExpenseAccountDTO k) {
+		return expenseAccountService.save(k);
+	}
 	
+	@PatchMapping
+	public ExpenseAccountDTO update(@RequestBody ExpenseAccountDTO k) {
+		return this.expenseAccountService.save(k);
+	}
+	
+	@DeleteMapping("/delete-expense-account/{id}")
+	public void deleteExpenseAccount(@PathVariable long id) {
+		this.expenseAccountService.deleteExpenseAccount(id);
+	}
 }
