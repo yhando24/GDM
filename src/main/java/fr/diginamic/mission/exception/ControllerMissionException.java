0c20acfeb376f8aa4b6f7e01a8093754e83156c4
unknown <alex.ayato@gmail.com>
2019-04-11 10:30:29 +0200

    gestion des excpetions mission

---
+++ src/main/java/fr/diginamic/mission/exception/ControllerMissionException.java
@@ -0,0 +1,30 @@
+package fr.diginamic.mission.exception;
+
+public class ControllerMissionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4301377981577609153L;
+
+	public ControllerMissionException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerMissionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerMissionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerMissionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
