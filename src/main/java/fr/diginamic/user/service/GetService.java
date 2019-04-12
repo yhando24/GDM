c7f5f0ddb6454db8e716bb8c982b33965ec96a3d
anisskiller <anissaallou36@gmail.com>
2019-04-12 13:48:36 +0200

    l

---
+++ src/main/java/fr/diginamic/user/service/GetService.java
@@ -0,0 +1,33 @@
+package fr.diginamic.user.service;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class GetService {
+	
+	
+
+	static final String URL_ABSENCES = "http://localhost:8081/GDA/listAbsences";
+	
+	public static void main(String[] args) {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+        // Request to return JSON format
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("my_other_key", "my_other_value");
+			HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		  RestTemplate restTemplate = new RestTemplate();
+		  
+		  ResponseEntity<String> response = restTemplate.exchange(URL_ABSENCES, 
+	                HttpMethod.GET, entity, String.class);
+	}
+}
